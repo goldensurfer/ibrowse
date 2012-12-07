547a675b837c369aef08cdef4379551ae88a2a76
maruthavanan <maruthavanan.s@gmail.com>
2012-12-07 16:16:38 +0530

    Update src/ibrowse_lb.erl

--- src/ibrowse_lb.erl
+++ src/ibrowse_lb.erl
@@ -249,7 +249,7 @@ find_best_connection(Pid, Tid, Max_pipe) ->
                 _ ->
                     {ok, Pid}
             end;
-_ ->
+         _ ->
             find_best_connection(ets:next(Tid, Pid), Tid, Max_pipe)
     end.
 
